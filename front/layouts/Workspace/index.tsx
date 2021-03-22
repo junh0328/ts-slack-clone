8d3c5b27cb68259b2b3e08e10281f74a67f18400
이준희 <junh0328@naver.com>
2021-03-22 16:10:12 +0900

    feat : 슬랙 워크스페이스 UI 구성중

---
+++ front/layouts/Workspace/index.tsx
@@ -0,0 +1,61 @@
+import React, { FC, useCallback } from 'react';
+import fetcher from '@utils/fetcher';
+import useSWR from 'swr';
+import axios from 'axios';
+import { Redirect } from 'react-router';
+import gravatar from 'gravatar';
+
+import {
+  Channels,
+  Chats,
+  Header,
+  MenuScroll,
+  ProfileImg,
+  RightMenu,
+  WorkspaceName,
+  Workspaces,
+  WorkspaceWrapper,
+} from '@layouts/Workspace/styles';
+
+const Workspace: FC = ({ children }) => {
+  const { data, error, revalidate, mutate } = useSWR('http://localhost:3095/api/users', fetcher, {
+    dedupingInterval: 2000,
+  });
+
+  const onLogout = useCallback(() => {
+    axios
+      .post('http://localhost:3095/api/users/logout', null, {
+        withCredentials: true,
+      })
+      .then(() => {
+        mutate(false, false);
+        // redux-saga에서 me 와 같은 나의 정보가 들은 state의 역할을 SWR에서는 useSWR()이 하게 됨
+      });
+  }, []);
+
+  if (!data) {
+    return <Redirect to="/login" />;
+  }
+  return (
+    <div>
+      <Header>
+        <RightMenu>
+          <span>test</span>
+          <ProfileImg src={gravatar.url(data.email, { s: '28px', d: 'retro' })} alt={data.email} />
+        </RightMenu>
+      </Header>
+      <button onClick={onLogout}>로그아웃</button>
+      <WorkspaceWrapper>
+        <Workspaces>test</Workspaces>
+        <Channels>
+          <WorkspaceName>Sleact</WorkspaceName>
+          <MenuScroll>menu scroll</MenuScroll>
+        </Channels>
+        <Chats>Chats</Chats>
+      </WorkspaceWrapper>
+      {children}
+    </div>
+  );
+};
+
+export default Workspace;
