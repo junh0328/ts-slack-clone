543368537eccdfb84898fc195a9b22a07c311436
이준희 <junh0328@naver.com>
2021-03-24 17:10:13 +0900

    feat: ChatBox 및 DM에 기본적인 폼 구성 완료

--- front/pages/DirectMessage/index.tsx
+++ front/pages/DirectMessage/index.tsx
@@ -1,7 +1,38 @@
-import React from 'react';
+import { Container, Header } from '@pages/Channel/styles';
+import React, { useCallback } from 'react';
+import gravatar from 'gravatar';
+import useSWR from 'swr';
+import fetcher from '@utils/fetcher';
+import { useParams } from 'react-router';
+import ChatBox from '@components/ChatBox';
+import ChatList from '@components/ChatList';
+import useInput from '@hooks/useInput';
 
 const DirectMessage = () => {
-  return <div>DM 페이지</div>;
+  const { workspace, id } = useParams<{ workspace: string; id: string }>();
+
+  const { data: userData } = useSWR(`http://localhost:3095/api/workspaces/${workspace}/users/${id}`, fetcher);
+  const { data: myData } = useSWR(`http://localhost:3095/api/users`, fetcher);
+  const [chat, onChangeChat] = useInput('');
+
+  const onSubmitForm = useCallback((e) => {
+    e.preventDefault();
+    alert('submit!');
+  }, []);
+
+  if (!userData || !myData) {
+    return <div>로딩중 ...</div>;
+  }
+  return (
+    <Container>
+      <Header>
+        <img src={gravatar.url(userData.email, { s: '24px', d: 'retro' })} alt={userData.nickname} />
+        <span>{userData.nickname}</span>
+      </Header>
+      <ChatList />
+      <ChatBox chat={chat} onSubmitForm={onSubmitForm} onChangeChat={onChangeChat} />
+    </Container>
+  );
 };
 
 export default DirectMessage;
